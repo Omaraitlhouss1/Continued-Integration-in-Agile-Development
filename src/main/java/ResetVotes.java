3e60e0735819d3c575be38569926b2d7abf4d217
Omaraitlhouss1 <159189285+Omaraitlhouss1@users.noreply.github.com>
2024-02-28 10:16:07 +0100

    Update ResetVotes.java

--- src/main/java/ResetVotes.java
+++ src/main/java/ResetVotes.java
@@ -2,7 +2,7 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.sql.*;
-
+ 
 public class ResetVotes extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html"); 
